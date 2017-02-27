2f5eec3c95b82d981bcbdfddba48c9ca4b9bfd05
Artyom <artyomkagramanov@gmail.com>
2017-02-27 21:27:02 +0400

    inputs

--- src/components/data/Calculator.js
+++ src/components/data/Calculator.js
@@ -2,13 +2,13 @@ import Units from './Units.js';
 import Bedrooms from './Bedrooms.js';
 
 function Calculator(inputs) {
-
-  let result = {};
-  result['Calculator for new appliances'] = {};
-  result['Calculator for new appliances']['Costs'] = {};
-  result['Calculator for new appliances']['Costs']['Cost of Electricity - Washing Machine']  = {};
-  result['Calculator for new appliances']['Costs']['Cost of Electricity - Washing Machine']['Per load']  = {};
-  return resuls;
+	let {sqft, no_of_bedrooms, percentage_use_laundry, price_per_load_washing_machine, price_per_hour_dryer} = inputs;
+	let result = {};
+	result['Calculator for new appliances'] = {};
+	result['Calculator for new appliances']['Costs'] = {};
+	result['Calculator for new appliances']['Costs']['Cost of Electricity - Washing Machine']  = {};
+	result['Calculator for new appliances']['Costs']['Cost of Electricity - Washing Machine']['Per load']  = {};
+	return result;
 }
 
 export default Calculator;
